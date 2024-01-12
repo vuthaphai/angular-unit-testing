67fcf8f21eb909936a9a98a5a20791b66dfea38b
Vutha Phai (ផៃ វុត្ថា) <vutha_phai@manulife.com>
2024-01-12 17:24:43 +0700

    finished: testing rxjs

--- src/app/shared/services/users.service.spec.ts
+++ src/app/shared/services/users.service.spec.ts
@@ -1,27 +1,16 @@
 import { UserInteface } from '../types/user.interface';
 import { UsersService } from './users.service';
 import { TestBed } from '@angular/core/testing';
-import { UtilsService } from './utils.service';
 
 describe('UsersService', () => {
   let usersService: UsersService;
-  let utilsService: UtilsService;
-
-  // const utilsServiceMock = {
-  //   pluck: jest.fn(),
-  // };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        UsersService,
-        UtilsService,
-        // { provide: UtilsService, useValue: utilsServiceMock },
-      ],
+      providers: [UsersService],
     });
 
     usersService = TestBed.inject(UsersService);
-    utilsService = TestBed.inject(UtilsService);
   });
 
   it('creates a service', () => {
@@ -35,30 +24,17 @@ describe('UsersService', () => {
         name: 'foo',
       };
       usersService.addUser(user);
-      expect(usersService.users).toEqual([{ id: '3', name: 'foo' }]);
+      expect(usersService.users$.getValue()).toEqual([
+        { id: '3', name: 'foo' },
+      ]);
     });
   });
 
   describe('removeUser', () => {
     it('should remove a user', () => {
-      usersService.users = [{ id: '3', name: 'foo' }];
+      usersService.users$.next([{ id: '3', name: 'foo' }]);
       usersService.removeUser('3');
-      expect(usersService.users).toEqual([]);
-    });
-  });
-
-  describe('getUsername', () => {
-    it('should get usernames', () => {
-      jest.spyOn(utilsService, 'pluck');
-      usersService.users = [{ id: '3', name: 'foo' }];
-      usersService.getUsernames();
-      expect(utilsService.pluck).toHaveBeenCalledWith(
-        usersService.users,
-        'name'
-      );
-
-      // utilsServiceMock.pluck.mockReturnValue(['foo']);
-      // expect(usersService.getUsernames()).toEqual(['foo']);
+      expect(usersService.users$.getValue()).toEqual([]);
     });
   });
 });
