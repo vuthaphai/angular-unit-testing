d02b694a6c51dabbb219a6f90228c3bd16979679
vuthaphai <vuthaphai@gmail.com>
2024-01-14 16:30:21 +0700

    finsihed: e2e testing

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,127 @@
 describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
+  beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3004/todos'
+    }, {
+      body: [
+        {
+          "text": "first todo",
+          "isCompleted": true,
+          "id": 1
+        },
+        {
+          "text": "second todo",
+          "isCompleted": false,
+          "id": 2
+        },
+        {
+          "text": "third todo",
+          "isCompleted": false,
+          "id": 3
+        },
+        
+      ]
+    }).intercept({
+      method: 'POST',
+      url: 'http://localhost:3004/todos'
+    },
+    {
+      body: { text: 'foo', id: 4, isCompleted: false}
+    }
+    ).intercept({
+      method: 'DELETE',
+      url: 'http://localhost:3004/todos/1'
+    },
+    {
+      body: {}
+    }
+    ).intercept({
+      method: 'PATCH',
+      url: 'http://localhost:3004/todos/1'
+    },
+    {
+      body: {text: 'foo', id: 1, isCompleted: false}
+    }
+    )
+    .visit('/')
   })
+  
+  it('Visits the initial project page', () => {
+    cy.contains('todos')
+  })
+
+  it('renders 3 todos', () => {
+    cy.get('[data-cy="todo"]').should('have.length',3)
+    cy.get('[data-cy="todoLabel"]').eq(0).should('contain.text','first todo')
+    cy.get('[data-cy="todoLabel"]').eq(1).should('contain.text','second todo')
+    cy.get('[data-cy="todoLabel"]').eq(2).should('contain.text','third todo')
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('be.checked')
+  })
+
+  it('renders footer', () => {
+    cy.get('[data-cy="todoCount"]').should('contain.text','2 items left')
+    cy.get('[data-cy="filterLink"]')
+      .eq(0)
+      .should('contain.text','All')
+      .should('have.class','selected')
+    cy.get('[data-cy="filterLink"]').eq(1).should('contain.text','Active')
+    cy.get('[data-cy="filterLink"]').eq(2).should('contain.text','Completed')
+  })
+
+  it('can change filter', () => {
+    cy.get('[data-cy="filterLink"]')
+      .eq(1).click()
+    cy.get('[data-cy="filterLink"]')
+      .eq(1)
+      .should('have.class','selected')
+  })
+
+  it('can add todo', ()=> {
+    cy.get('[data-cy="newTodoInput"]').type('foo{enter}')
+    cy.get('[data-cy="todoCount"]').should('contain.text','3 items left')
+    cy.get('[data-cy="todoLabel"]').eq(3).should('contain.text','foo')
+  })
+
+  it('can remove todo', () => {
+    cy.get('[data-cy="destroy"]').eq(0).click({force: true})
+    cy.get('[data-cy="todo"]').should('have.length',2)
+  })
+
+  it('can toggle a todo', () => {
+    cy.get('[data-cy="todoCheckbox"]').eq(0).click()
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('not.be.checked')
+  })
+
+  it('can toggle all todo', () => {
+    cy.intercept({
+      method: 'PATCH',
+      url: 'http://localhost:3004/todos/*'
+    },
+    {
+      body: { text: 'foo', id: 1, isCompleted: true}
+    }
+    )
+    cy.get('[data-cy="toggleAll"]').click()
+    cy.get('[data-cy="todoCheckbox"]').eq(0).should('be.checked')
+    cy.get('[data-cy="todoCheckbox"]').eq(1).should('be.checked')
+    cy.get('[data-cy="todoCheckbox"]').eq(2).should('be.checked')
+  })
+
+  it('shold update a todo', () => {
+    cy.intercept({
+      method: 'PATCH',
+      url: 'http://localhost:3004/todos/1'
+    },
+    {
+      body: { text: 'bar', id: 1, isCompleted: false}
+    }
+    )
+
+    cy.get('[data-cy="todoLabel"]').eq(0).dblclick()
+    cy.get('[data-cy="todoEdit"]').type('bar{enter}')
+    cy.get('[data-cy="todoLabel"]').eq(0).should('contain.text', 'bar')
+
+  })
+
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from "./todos/todos.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, TodosComponent]
 })
 export class AppComponent {
   title = 'angular-unit-testing';
